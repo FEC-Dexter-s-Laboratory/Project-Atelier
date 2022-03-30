60b242c138dec49c298f3b8d7872cbb37b66c499
ivykw <ivyw219@gmail.com>
2022-03-30 13:58:00 -0500

    set up package.json, webpack, babel

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development"
+  entry: path.join(__dirname, "./client/src/index.js",
+  output: {
+    path: path.join(__dirname, "client/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+  dev-tool: "source-map"
+}
