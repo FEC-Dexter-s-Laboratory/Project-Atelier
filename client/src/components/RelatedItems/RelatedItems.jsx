e5d5a120eb0407ecd4f7e11134faab014f6e36b7
ivykw <ivyw219@gmail.com>
2022-03-31 16:22:22 -0500

    work on axios requests for related items

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import RelatedCard from './RelatedCard.jsx';
+const axios = require('axios');
 
 const DivContainer = styled.div`
   border: 6px ridge darkblue;
@@ -8,29 +9,58 @@ const DivContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
 `;
-// in app, make GET /products/:product_id/related for related product ids, then
-// make GET /products/:product_id for name, category, and GET /products/:product_id/styles
-// for item where "default"?" is true
+// make GET /products/:product_id/related for related product ids, then
+// make GET /products/:product_id for name, category, and
+// GET /products/:product_id/styles for item where "default"?"
 // is true and extract the original and sale price, and the photo/thumbnail url
-// props received is an array of objects
-const RelatedItems = (props) => {
+
+const RelatedItems = function(props) {
   // function handlers, state, hooks, general javascript all goes here
-  var testData = [
-    {
-      name: "Air Minis 250",
-      category: "Basketball Shoes",
-    },
-    {
-      name: "Camo Onesie",
-      category: "Jackets",
-      thumbnail: "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    },
-  ]
-  const [isClicked, setIsClicked] = useState(false);
+  // var testData = [
+  //   {
+  //     name: "Air Minis 250",
+  //     category: "Basketball Shoes",
+  //   },
+  //   {
+  //     name: "Camo Onesie",
+  //     category: "Jackets",
+  //     thumbnail: "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+  //   },
+  // ]
+  const [state, setState] = useState([]);
 
   useEffect(() => {
-    // implement desired hook effects here
-  }, [isClicked]);
+    let relatedProducts = [];
+    // GET related ids
+    axios.get(`/products/${props.currentId}/related`)
+      .then((relatedIds) => {
+        // for each id, GET product details AND product style details for price
+        for (let i = 0; i < relatedIds.length; i++) {
+          let product = {};
+          axios.get(`/products/${relatedIds[i]}`)
+            .then((productDetails) => {
+              product.id = productDetails.id;
+              product.name = productDetails.name;
+              product.category = productDetails.category;
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+            .then((
+              axios.get(`/products/${relatedIds[i]}/styles`)
+                .then((styles) => {
+                  product.original_price = styles[0].original_price;
+                  product.sale_price = styles[0].sale_price;
+                  product.photos = styles[0].photos;
+                  product.push(relatedProducts);
+                })
+                .catch((err) => {
+                  console.log(err);
+                })
+            ));
+        }
+      });
+  }, [state]);
 
   return (
     <DivContainer>
