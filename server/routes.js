377cb1c4a1c74e369af1436b8182233f387b1782
ivykw <ivyw219@gmail.com>
2022-03-31 16:56:23 -0500

    server routes: added authorization header for general route, made related products route

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 
 const router = express.Router();
 
@@ -7,7 +8,11 @@ const router = express.Router();
 
 // example route for /products
 router.get('/products', (req, res) => {
-  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products')
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products', {
+    headers: {
+      Authorization: process.env.AUTH_TOKEN,
+    },
+  })
     .then(({ data }) => {
       res.send(data);
     })
@@ -18,5 +23,19 @@ router.get('/products', (req, res) => {
 });
 
 // other routes here
+router.get('/products/:product_id/related', (req, res) => {
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${req.params.product_id}/related`, {
+    headers: {
+      Authorization: process.env.AUTH_TOKEN,
+    },
+  })
+    .then(({ data }) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(404);
+    });
+});
 
 module.exports = router;
