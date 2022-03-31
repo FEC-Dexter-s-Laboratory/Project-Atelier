8c28fac3fb43db0101f23d5c20ae855969057068
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-03-30 22:33:16 -0600

    Created a new Search component, fleshed out it's skeleton, gave it styled components which will need fine tuning, gave it state and effect, created a submitSearch function, and put the pieces together to render. I also made sure to import it into the App component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Overview from './Overview.jsx';
 import RatingsAndReviews from './RatingsAndReviews.jsx';
 import RelatedItems from './RelatedItems.jsx';
+import Search from './Search.jsx';
 import QAndA from './QAndA.jsx';
 
 const App = () => {
---
+++ client/src/components/Search.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const DivContainer = styled.div`
+  width: 100%;
+  position: fixed;
+  height: 5%;
+  background-image: linear-gradient(to right, deepskyblue, cyan);
+  display: flex;
+`;
+
+const SearchInput = styled.input`
+  justify-content: flex-end;
+  border: 1px solid blue;
+  border-radius: 12px;
+`;
+
+const SearchButton = styled.button`
+  background-image: url(https://st.depositphotos.com/1630589/4799/v/950/depositphotos_47996391-stock-illustration-magnifying-glass-icon.jpg);
+  background-size: cover;
+  background-position: center;
+  width: 5%;
+  height: 3%;
+  justify-content: flex-end;
+`
+
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const submitSearch = (query) => {
+    axios({
+      url: 'tbd',
+      method: 'POST',
+      data: {
+        query: query
+      }
+    })
+      .then(res => console.log('Successful Search POST -> ', res))
+      .catch(err => console.error('Submit Search Error -> ', err));
+  };
+
+  useEffect(() => {
+    submitSearch(searchTerm);
+  }, [searchTerm]);
+
+  return (
+    <DivContainer>
+      <SearchInput type='text' value={searchTerm} placeHolder='search here...' />
+      <SearchButton onClick={submitSearch} ></SearchButton>
+    </DivContainer>
+  )
+}
+
+export default Search;
