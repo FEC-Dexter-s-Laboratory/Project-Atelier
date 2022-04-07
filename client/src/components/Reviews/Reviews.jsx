3a15166c0854dbb7ae4957273dd6262a4dfb02d9
Kamran Masood <kam1088@gmail.com>
2022-04-06 19:12:44 -0700

    not matter

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -41,6 +41,142 @@ class Reviews extends React.Component {
       reviews: reviewData.results,
       meta: reviewMetaData
     };
+
+    this.fetchReviews = this.fetchReviews.bind(this);
+    this.fetchMeta = this.fetchMeta.bind(this);
+    this.filterReviewList = this.filterReviewList.bind(this);
+    this.displayMoreReviews = this.displayMoreReviews.bind(this);
+    this.setSort = this.setSort.bind(this);
+    this.toggleFilterReviews = this.toggleFilterReviews.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+
+
+  }
+
+  componentDidMount() {
+    this.fetchMeta()
+      .then(() => {
+        this.fetchReviews();
+      })
+      .then(() => {
+        this.render();
+      });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.currentId !== this.props.currentId) {
+      this.fetchMeta()
+        .then(() => {
+          return this.fetchReviews();
+        })
+        .then(() => this.render());
+    } else if (prevState.productId !== this.props.productId) {
+      this.fetchMeta()
+        .then(() => {
+          return this.fetchReviews();
+        })
+        .then(() => this.render());
+    } else if ( prevState.sort !== this.state.sort) {
+      this.fetchReviews()
+        .then(() => this.render());
+    } else {
+      this.render();
+    }
+  }
+
+  fetchReviews() {
+    return axios.get(`/reviews/${this.state.productId}`, {
+      params: {
+        count: this.state.count,
+        sort: this.state.sort
+      }
+    })
+      .then(({data}) => {
+        this.setState({
+          reviews: data.results
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  fetchMeta() {
+    return axios.get(`/reviews/meta/${this.state.productId}`)
+      .then(({data}) => {
+        let count = 0;
+        for (let key in data.ratings) {
+          count += Number(data.ratings[key]);
+        }
+        this.setState({
+          meta: data,
+          count: count,
+          loading: false
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  filterReviewList(reviews) {
+    let filtered = [];
+    for (let review of reviews) {
+      if (this.state.filterReviews.includes(review.rating)) {
+        filtered.push(review);
+      }
+    }
+    return filtered.slice(0, this.state.reviewsToDisplay);
+  }
+
+  displayMoreReviews() {
+    let more = this.state.reviewsToDisplay + 2;
+    this.setState({
+      reviewsToDisplay: more
+    });
+    if (this.state.reviews.length === 2) {
+      this.fetchReviews();
+    }
+  }
+
+  setSort(e) {
+    e.preventDefault();
+    this.setState({
+      sort: e.target.value
+    });
+  }
+
+  toggleFilterReviews(stars) {
+
+    if (stars === 'reset') {
+      this.setState({
+        filterReviews: [1, 2, 3, 4, 5],
+        resetFilters: false
+      });
+      return;
+    }
+
+    let currentFilters = this.state.filterReviews.slice();
+    if (currentFilters.length === 5) {
+      currentFilters = [stars];
+    } else if (currentFilters.includes(stars)) {
+      currentFilters.splice(currentFilters.indexOf(stars), 1);
+    } else {
+      currentFilters.push(stars);
+    }
+    this.setState({
+      filterReviews: currentFilters,
+      resetFilters: true
+    });
+
+    // temp fix
+    if (this.state.reviews.length === 2) {
+      this.fetchReviews();
+    }
+  }
+
+  toggleModal() {
+    this.setState({
+      addReview: !this.state.addReview
+    });
   }
 
   render() {
