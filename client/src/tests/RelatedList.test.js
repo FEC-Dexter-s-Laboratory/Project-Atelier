f0a4bb792d87ba98a09eb3ceefcd835b97d0db2c
ivykw <ivyw219@gmail.com>
2022-04-08 21:25:31 -0500

    saving to merge new main

--- client/src/tests/RelatedList.test.js
+++ client/src/tests/RelatedList.test.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import { render, screen } from '@testing-library/react';
 import RelatedList from '../components/RelatedItems/RelatedList.jsx';
+import OutfitList from '../components/RelatedItems/OutfitList.jsx';
+import Card from '../components/RelatedItems/Card.jsx';
 
 describe('RelatedList', () => {
   test('renders Related List component', () => {
     render(<RelatedList />);
+    expect(screen.getByRole('heading', {name: 'Related Items'})).toExist;
+  });
+});
+
+// test if related cards are rendered
+describe('RelatedCards', () => {
+  test('renders Related Card components', () => {
+    // render(<Card />);
+    // screen.debug;
   });
 });
 
 // test if outfit list renders
-
-// test if cards render
+describe('OutfitList', () => {
+  test('renders Outfit List component', () => {
+    render(<OutfitList />);
+    expect(screen.getByRole('heading', {name: 'Your Outfit'})).toExist;
+  });
+});
 
 // test if outfits display in order added
